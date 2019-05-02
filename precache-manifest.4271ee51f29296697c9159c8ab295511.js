dabe5d19d129fe66e93b0cef0627a18b410dc6d3
Alejandro Jimenez <ajim4268@github.com>
2019-05-01 21:59:56 -0600

    Updates

---
+++ precache-manifest.4271ee51f29296697c9159c8ab295511.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "13fc78e111fc73b3816c",
+    "url": "/projects/static/js/runtime~main.13fc78e1.js"
+  },
+  {
+    "revision": "b2ce21f494e454844d4e",
+    "url": "/projects/static/js/main.b2ce21f4.chunk.js"
+  },
+  {
+    "revision": "3752c91fbf3dc44f7602",
+    "url": "/projects/static/js/2.3752c91f.chunk.js"
+  },
+  {
+    "revision": "b2ce21f494e454844d4e",
+    "url": "/projects/static/css/main.5222a04c.chunk.css"
+  },
+  {
+    "revision": "643f13d11315819873db59411fc58fa3",
+    "url": "/projects/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/projects/precache-manifest.b1b96737f0d909855929cf3ca9c4378b.js"
+  "/projects/precache-manifest.4271ee51f29296697c9159c8ab295511.js"
 );
 
 workbox.clientsClaim();
